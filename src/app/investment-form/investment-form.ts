4bdfd2a096c755d510a2a95d2d897b4df6826566
Amir Azuz <natural6t@gmail.com>
2025-09-25 11:38:19 +0200

    Add main form logic

--- src/app/investment-form/investment-form.ts
+++ src/app/investment-form/investment-form.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, effect, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+export enum InvestmentType {
+  Lump = 'LUMP',
+  Recurring = 'RECURRING'
+}
 
 @Component({
   selector: 'app-investment-form',
-  imports: [],
+  imports: [FormsModule, CurrencyPipe],
   templateUrl: './investment-form.html',
   styleUrl: './investment-form.css'
 })
 export class InvestmentForm {
+  InvestmentType = InvestmentType; // to get access in HTML
+  selectedInvestmentType = signal<InvestmentType>(InvestmentType.Lump)
 
+  enteredStartingDate = signal("")
+  enteredInitialInvestmentAmount = signal(10000);
+  enteredRecurringContribution = signal(100)
+
+  // Clear input ans save
+  onAmountChange(value: string, signalToChange: WritableSignal<Number>) {
+    const numeric = parseFloat(value.replace(/[^0-9.]/g, ''));
+    signalToChange.set(isNaN(numeric) ? 0 : numeric);
+  }
+
+
+  onSubmit() {
+    console.log('Selected starting date:', this.enteredStartingDate());
+    console.log('Selected type:', this.selectedInvestmentType());
+    console.log('Selected initial investment amount:', this.enteredInitialInvestmentAmount());
+    console.log('Selected recurring contribution:', this.enteredRecurringContribution());
+  }
 }
